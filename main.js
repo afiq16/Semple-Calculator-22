acec8d1aaa9e92fabbce28259dfd5512f8943da7
afiq16 <106015528+afiq16@users.noreply.github.com>
2022-09-19 18:26:53 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+  function fun()
+         {
+           
+  var a=parseInt(document.getElementById('as').value);
+  var b=parseInt(document.getElementById('ad').value);
+  document.getElementById('result').innerHTML=a+b;
+         }
+ function faun()
+         {
+           
+  var a=parseInt(document.getElementById('as').value);
+  var b=parseInt(document.getElementById('ad').value);
+  document.getElementById('result').innerHTML=a-b;
+         }
+
+        function fan()
+         {
+           
+  var a=parseInt(document.getElementById('as').value);
+  var b=parseInt(document.getElementById('ad').value);
+  document.getElementById('result').innerHTML=a*b;
+         }
+
+        function fua()
+         {
+           
+  var a=parseInt(document.getElementById('as').value);
+  var b=parseInt(document.getElementById('ad').value);
+  document.getElementById('result').innerHTML=a/b;
+ 
+  
+         }
+
